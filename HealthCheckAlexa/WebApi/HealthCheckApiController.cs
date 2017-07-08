d24c37f6fade99e4d5ab70956172deeccdf87899
OptimisticCoder <danpatching@gmail.com>
2017-07-08 17:38:55 +0100

    Trying to return statuses for smtp health check

--- HealthCheckAlexa/WebApi/HealthCheckApiController.cs
+++ HealthCheckAlexa/WebApi/HealthCheckApiController.cs
@@ -2,14 +2,43 @@
 {
     using Alexa.Models;
     using Alexa.Models.SubModels;
+    using System.Net.Http;
     using System.Web.Http;
     using Umbraco.Web.WebApi;
+    using Newtonsoft.Json;
+    using System.Text;
+    using Umbraco.Web.HealthCheck;
+    using Umbraco.Web.HealthCheck.Checks.Services;
+    using System.Linq;
 
-    public class HealthCheckApiController : UmbracoAuthorizedApiController
+    public class HealthCheckApiController : UmbracoApiController
     {
+        
         [HttpPost]
-        public AlexaResponse DoHealthChecks([FromBody]AlexaRequest request)
+        public HttpResponseMessage DoHealthChecks([FromBody]AlexaRequest request)
         {
+            var context = new HealthCheckContext(UmbracoContext.HttpContext, UmbracoContext);
+
+            var smtp = new SmtpCheck(context);
+            var statuses = smtp.GetStatus();
+
+            var sb = new StringBuilder();
+            var index = 0;
+            foreach(var s in statuses)
+            {
+                if (sb.Length == 0)
+                    sb.Append(s.Message);
+                else if (index == statuses.Count() - 1 && statuses.Count() > 1)
+                    sb.Append(", and " + s.Message);
+                else
+                    sb.Append(", " + s.Message);
+
+                index++;
+            }
+
+            if (sb.Length == 0)
+                sb.Append("The health check returned no results.");
+
             var response = new AlexaResponse
             {
                 Version = "1.0",
@@ -18,19 +47,24 @@
                     OutputSpeech = new OutputSpeech
                     {
                         Type = "PlainText",
-                        Text = "Hello Alexa world!"
+                        Text = sb.ToString()
                     },
                     Card = new Card
                     {
                         Type = "Simple",
-                        Title = "Title of the card",
-                        Content = "Hello Alexa card world"
+                        Title = "Health Check Results",
+                        Content = sb.ToString()
                     }
                 },
                 ShouldEndSession = true
             };
 
-            return response;
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(response), 
+                                            Encoding.UTF8, 
+                                            "application/json")
+            };
         }
     }
 }
